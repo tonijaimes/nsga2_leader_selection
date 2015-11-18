d6b3633195e636c937fa7e6b7046cc455cd8cbc2
Abel García Nájera <abel@imac-de-abel.local>
2015-11-18 15:31:20 -0600

    Agregué dos archivos

---
+++ NSGA2++/src/prueba.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Prueba
+//
+//  Created by Abel García Nájera on 18/11/15.
+//  Copyright © 2015 Abel García Nájera. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
